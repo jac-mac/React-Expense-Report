b7944fe473a8e30c212493475449ebc27ea0369e
jac-mac <jackrmcclure@gmail.com>
2021-04-26 11:35:11 -0500

    Added dynamic list rendering

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -4,6 +4,7 @@ export default function ExpenseForm(props) {
     const [enteredTitle, setTitle] = useState('')
     const [enteredAmount, setAmount] = useState('')
     const [enteredDate, setDate] = useState('')
+    const [showForm, setShowForm] = useState(false)
 
     const titleHandler = (event) => {
         setTitle(event.target.value)
@@ -50,6 +51,7 @@ export default function ExpenseForm(props) {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,6 +1,8 @@
 import './NewExpense.css'
 import ExpenseForm from './ExpenseForm'
+import { useState } from 'react'
 export default function NewExpense(props) {
+    const [showForm, setShowForm] = useState(false)
 
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -9,9 +11,18 @@ export default function NewExpense(props) {
         }
         props.onAddExpense(expenseData)
     }
+
+    const showFormHandler = () => {
+        setShowForm(true)
+    }
+
+    const stopShowHandler = () => {
+        setShowForm(false)
+    }
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+            {showForm && <ExpenseForm onCancel={stopShowHandler} onSaveExpenseData={saveExpenseDataHandler}/>}
         </div>
     )
 }
